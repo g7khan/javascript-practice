5fe14f351ab7ccca6e40300e23dc63eed3599423
g7khan <owaissattar296@gmail.com>
2024-06-01 13:27:35 -0700

    javascript practice work

---
+++ app.js
@@ -0,0 +1,223 @@
+// let todoInput = document.getElementById('todoInput')
+// let ul = document.getElementById("ul")
+// function addTodo(){
+//     let list = document.createElement('li')
+//     let deleteButton = document.createElement("button")
+
+//     let updatebutton = document.createElement("button")
+//     list.innerText= todoInput.value
+
+
+//     list.innerText= todoInput.value
+//     ul.appendChild(list)
+//     list.appendChild(deleteButton)
+//     list.appendChild(updatebutton)
+//     deleteButton.innerText= 'delete'
+//     updatebutton.innerText= 'update'
+//     list.classList.add('list')
+// }
+
+
+// ul.addEventListener('click',(e)=>{
+// if(e.target.innerText == "delete"){
+//   let listItem = e.target.parentElement;
+//         listItem.remove();
+//     console.log('deleteclick')
+// }
+// else if(e.target.innerText == "update"){
+//    let listItem= e.target.parentElement
+//    let newVal = prompt('Enter new value')
+//    if(newVal !== ''){
+//     listItem.firstChild.textContent = newVal
+//    }
+//     console.log('updateclick')
+// }
+
+// })
+
+
+
+// let user =[{
+
+//     Name : "owais",
+//     faherName : "Abdul sattar",
+//     section : "B",
+//     Rollno : "12345",
+//     Department : "computer science",
+
+
+// },{
+
+//     Name : "aleem",
+//     faherName : "aleem shoukat",
+//     section : "B",
+//     Rollno : "12345",
+//     Department : "computer science",
+
+
+// }]
+
+// for (const owais of user) {
+//     console.log(user[owais]);
+// }
+
+// user.forEach((val)=>{
+//     console.log(val.Name);
+//     console.log(val.Rollno);
+//     console.log(val.faherName);
+//     console.log(val.section);
+// })
+
+//  for (const owais in user) {
+//     console.log(owais ,user[owais]);
+//  }
+// console.log(user);
+
+
+
+
+// let arr = [1,2,3,4,5]
+
+// arr.unshift(0.5)
+
+// let secondarr = arr.splice(2,0,2.5,2.9)
+// console.log(arr);
+
+
+
+// let fruit = ['banana','apple','mango']
+// let secondarr = [1,2,3]
+
+// let third = [...fruit,...secondarr]
+// console.log(third);
+
+
+
+
+// let user = [{
+
+//     Name : "Owais",
+//     FatherNamne : "Abdulsattar",
+//     Cnic : "42301-8036498-5",
+//     Phonenumber : "0319-8268445",
+//     Email : "owaissattar296@gmail.com",
+//     Dob : "01/10/2004",
+
+// },
+// {
+
+
+//     Name : "Owais",
+//     FatherNamne : "Abdulsattar",
+//     Cnic : "42301-8036498-5",
+//     Phonenumber : "0319-8268445",
+//     Email : "owaissattar296@gmail.com",
+//     Dob : "01/10/2004",
+
+// },
+// {
+
+
+
+
+//     Name : "Owais",
+//     FatherNamne : "Abdulsattar",
+//     Cnic : "42301-8036498-5",
+//     Phonenumber : "0319-8268445",
+//     Email : "owaissattar296@gmail.com",
+//     Dob : "01/10/2004",
+
+
+// }]
+
+
+
+
+// // for (const Owais of user) {
+// //     console.log(user[Owais]);
+// // }
+
+
+//   for (const owais in user) {
+//     console.log(owais ,user[owais]);
+//  }
+// console.log(user);
+
+
+
+
+// for (const owais of listItem) {
+//     console.log(listItem,[owais]);
+// }
+
+// listItem.forEach((val)=>{
+
+// console.log(val.Name);
+// console.log(val.FatherNamne);
+// console.log(val.Cnic);
+// console.log(val.Phonenumber);
+// console.log(val.Email);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     let arr = [1,2,3,4,5,7,8,9,10,11,23,56,34]
+
+// arr.unshift(0.66)
+
+// console.log(arr);
+
+// let secondarr = arr.splice(2,0,2.5,2.9)
+// console.log(arr);
+
+
+
+// let fruit = ['banana','apple','mango']
+// let secondarr = [1,2,3]
+
+// let third = [...fruit,...secondarr]
+// console.log(third);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
